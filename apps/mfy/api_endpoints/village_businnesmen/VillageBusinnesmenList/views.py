58a246edad1dada850351c7b871fab3d74cee10e
y.abdurahimov <y.abdurahimov@uic.best>
2024-04-01 11:02:18 +0500

    feat: add refreshapi and set access token limit

--- apps/mfy/api_endpoints/village_businnesmen/VillageBusinnesmenList/views.py
+++ apps/mfy/api_endpoints/village_businnesmen/VillageBusinnesmenList/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
 from apps.mfy.api_endpoints.village_businnesmen.VillageBusinnesmenList.serializers import \
     VillageBusinnesmenListSerilizer
@@ -8,6 +7,5 @@ from apps.mfy.models import VillageBusinnesmen
 
 class VillageBusinnesmenListAPIView(generics.ListAPIView):
     serializer_class = VillageBusinnesmenListSerilizer
-    permission_classes = (IsAuthenticated, )
     queryset = VillageBusinnesmen.objects.all()
 
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from apps.users.api_endpoints import user
 
 urlpatterns = [
     path("UserRegistration/", user.UserRegistrationAPIView.as_view(), name="UserRegistration"),
     path("Login/", TokenObtainPairView.as_view(), name="Login"),
+    path("TokenRefresh/", TokenRefreshView.as_view(), name="TokenRefresh"),
 ]
